85557765ac926ad900ad3fe0c6c5bc00f9797694
Arman Yessymkanov <arman.yessymkanov@nutanix.com>
2023-10-26 23:45:07 +0200

    remove dark styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,7 +11,7 @@ const CardComponent: FC<Props> = ({ card, isList }) => {
     return (
       <div className="w-full flex justify-between items-center px-3 py-2 border border-gray-300 rounded shadow-md bg-white">
         <div>
-          <h5 className="text-md font-medium tracking-tight text-gray-900 dark:text-white">
+          <h5 className="text-md font-medium tracking-tight text-gray-900">
             {card.title}
           </h5>
           <span className="font-normal text-sm">{card.team?.name}</span>
@@ -24,7 +24,7 @@ const CardComponent: FC<Props> = ({ card, isList }) => {
   }
   return (
     <div className="flex flex-col w-full block p-2 bg-white border-gray-300 rounded shadow-md bg-white border">
-      <h5 className="mb-2 text-md font-medium tracking-tight text-gray-900 dark:text-white">
+      <h5 className="mb-2 text-md font-medium tracking-tight text-gray-900">
         {card.title}
       </h5>
       <span className="font-normal text-sm">{card.status}</span>
